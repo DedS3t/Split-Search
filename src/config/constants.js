2695c0b91d623a269f6ddd0be4ae0feb64300bb3
DedS3t <61178879+DedS3t@users.noreply.github.com>
2021-02-07 01:22:28 -0800

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,14 +1,6 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyDLo9Ycf0EzlilhwiFh9Zah5LtiodZUNH8",
-  authDomain: "splitsearch-b0241.firebaseapp.com",
-  projectId: "splitsearch-b0241",
-  storageBucket: "splitsearch-b0241.appspot.com",
-  messagingSenderId: "73108573104",
-  appId: "1:73108573104:web:2bd6a8fe9db0d46d3f2475",
-  measurementId: "G-3FZ72RPMM3",
-  databaseURL:"https://splitsearch-b0241.firebaseio.com"
 }
 
 firebase.initializeApp(config)
